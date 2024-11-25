7aeb187c8a2512ee2da0c58d8a44f61fae79bf61
slugcraft <164267006+slugcraft@users.noreply.github.com>
2024-11-25 17:06:34 -0600

    Temp

---
+++ EXAMENMOVILESTests/EXAMENMOVILESTests.swift
@@ -0,0 +1,17 @@
+//
+//  EXAMENMOVILESTests.swift
+//  EXAMENMOVILESTests
+//
+//  Created by Miguel Angel Uribe Esquivel on 25/11/24.
+//
+
+import Testing
+@testable import EXAMENMOVILES
+
+struct EXAMENMOVILESTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
